d68dd4794f74700d6a8391feb75c4af2f453bc4f
can <can.kalender@mobuy.com.tr>
2022-02-22 22:17:59 +0300

    email and password textfields added.

--- lib/Login/login.dart
+++ lib/Login/login.dart
@@ -9,6 +9,19 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   bool toggleState=true;
+  String _errorText = "";
+  _getErrorText() {
+    return _errorText;
+  }
+
+  bool isEmail(String emailString) {
+    String emailRegexp =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+
+    RegExp regExp = RegExp(emailRegexp);
+
+    return regExp.hasMatch(emailString);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +32,24 @@ class _LoginState extends State<Login> {
       body: Center(
         child: Column(
           children: <Widget>[
+            Padding(
+                padding: EdgeInsets.only(top: 30,left: 20,right: 20),
+                child:TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter your e-mail',
+                  ),
+                ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20,left: 20,right: 20),
+              child:TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter your password',
+                ),
+              ),
+            ),
             Padding(
                 padding: EdgeInsets.only(top: 70.0),
                 child: ElevatedButton(
